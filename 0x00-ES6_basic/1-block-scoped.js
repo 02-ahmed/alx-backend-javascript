a5075c2c83dfea7e2f8b1d94466d1d5c97be0c28
02-ahmed <ahmedboabae2@gmail.com>
2024-01-04 05:26:02 +0000

    task 1 done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,8 +1,10 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
-  let task2 = true;
+  const task2 = true;
   if (trueOrFalse) {
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    return [task2, task];
   }
 
   return [task, task2];
